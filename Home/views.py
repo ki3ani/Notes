ae0ffd670bd28663a46f065c1e8b1ab70a4133ab
Kenneth Kimani <kimanim68@gmail.com>
2022-08-15 21:55:14 +0300

    validate forms

--- Home/views.py
+++ Home/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django import forms
 
 notes = ["chelsea","arsenal","bournemouth"]
+
+class NewNotesForm(forms.Form):
+    note = forms.CharField(label="New Notes")
 
 def index(request):
     return render(request, "Home/index.html", {
@@ -9,4 +13,17 @@ def index(request):
     })
 
 def add(request):
-    return render(request, "Home/add.html")
+    if request.method == "POST":
+        form = NewNotesForm(request.POST)
+        if form.is_valid():
+            note = form.cleaned_data["note"]
+            notes.append(note)
+
+        else:
+            return render(request, "Home/index.html", {
+                "form":form
+            })
+
+    return render(request, "Home/add.html", {
+        "form" : NewNotesForm()
+    })
