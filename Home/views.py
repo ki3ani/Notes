af9df4b244109c0cd503a724f840b73064c3f970
Kenneth Kimani <kimanim68@gmail.com>
2022-08-15 20:53:37 +0300

    paris

--- Home/views.py
+++ Home/views.py
@@ -1,13 +1,19 @@
-from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django import forms
 
 # Create your views here.
-notes = ["Had a good time chilling today","Ate sum pizza","went for swimming"]
+
+class AddNote(forms.Form):
+    note = forms.CharField(label="New Note")
+
 
 def index(request):
+    if "notes" not in request.session:
+        request.session["notes"] = []
+
     return render(request, "Home/index.html", {
-        "notes": notes
+        "notes": request.session["notes"]
     })
 
-    
